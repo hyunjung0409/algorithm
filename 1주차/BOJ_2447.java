45b99dda87e2d32ea7589378708ef684869f8b4e
Ginny <jasmin960427@naver.com>
2022-04-20 21:16:45 +0900

    fix BOJ/실버1/별찍기/함께풀이

--- 1주차/BOJ_2447.java
+++ 1주차/BOJ_2447.java
@@ -1,28 +1,47 @@
 import java.util.*;
 public class BOJ_2447 {
+	static String[][]arr;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
 		
-		star(N);
+		arr = new String[N][N];
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = " ";
+			}
+		}
+		
+		star(0, 0, N);
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}
 
 	}
 
-	private static void star(int n) {
+	private static void star(int r, int c, int n) {
 		if (n == 1) {
-			System.out.print("*");
+			arr[r][c] = "*";
 			
 			return;
 		}
 		
-		star(n / 3);
+		int cnt = 0;
+
 		
-		for (int i = 0 ; i < n; i++) {
-			System.out.print("*");
-			if (i == 5) {
-				System.out.print(" ");
+		for (int i = 0 ; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				cnt++;
+				if (cnt != 5) {
+					star(r + i * (n / 3) , c + j * (n / 3), n / 3);
+				}
 			}
 		}
 		

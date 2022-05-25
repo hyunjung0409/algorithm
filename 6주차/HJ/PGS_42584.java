5799aabe46ff1e453cfd977c75914797962a5d9c
이현정 <h.jung0409@daum.net>
2022-05-25 17:12:29 +0900

    fix PGS/레벨2/주식가격/15분

--- 6주차/HJ/PGS_42584.java
+++ 6주차/HJ/PGS_42584.java
@@ -1,30 +1,22 @@
-//프로그래머스 주식가격 레벨2 효율성 실패
-//프로그래머스 주식가격 레벨2 
+//프로그래머스 주식가격 레벨2 15분
 import java.io.*;
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Queue<Integer> input = new LinkedList<>();
         
         for(int i = 0 ; i < prices.length; i++ ){
-            for(int j = i ; j < prices.length; j++){
-                input.add(prices[j]);
-            }
-        
-            int standard = input.poll();
-            int cnt=0;
-            while(!input.isEmpty()){
+            int cnt = 0;
+            int standard = prices[i];
+            
+            for(int j = i+1 ; j < prices.length; j++){
                 cnt++;
-                if(input.poll() < standard){
+                if(prices[j] < standard ){
                     break;
                 }
-                
             }
-            
             answer[i] = cnt;
-            input.clear();
         }
         
         return answer;

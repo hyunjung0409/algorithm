4ffc282e46941e219179fccc532e958cab42ccfc
이현정 <h.jung0409@daum.net>
2022-09-14 23:30:22 +0900

    fix BOJ/실버1/스티커/1시간

--- 17주차/HJ/BOJ_9465.java
+++ 17주차/HJ/BOJ_9465.java
@@ -11,31 +11,24 @@ public class BOJ_9465 {
 		
 		for (int tc = 0; tc < t; tc++) {			
 			n = Integer.parseInt(br.readLine());
-			arr = new int[2][n];
+			arr = new int[2][n+1];
 			for (int i = 0; i < 2; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
-				for (int j = 0; j < n; j++) {
+				for (int j = 1; j < n+1; j++) {
 					arr[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			memo = new int[2][n];
-			memo[0][0] = arr[0][0];
-			memo[1][0] = arr[1][0];
-			
-			if(n >= 2) {				
-				memo[0][1] = Math.max(memo[1][0] + arr[0][1], arr[0][1]);
-				memo[1][1] = Math.max(memo[0][0] + arr[1][1], arr[1][1]);
-			}
+			memo = new int[2][n+1];
+			memo[0][1] = arr[0][1];
+			memo[1][1] = arr[1][1];
 
-			if(n >= 3) {
-				for (int i = 2; i < n; i++) {
-					memo[0][i] = Math.max(memo[1][i-2], memo[1][i-1]) + arr[0][i];
-					memo[1][i] = Math.max(memo[0][i-2], memo[0][i-1]) + arr[1][i];
-				}
+			for (int i = 2; i <= n; i++) {
+				memo[0][i] = Math.max(memo[1][i-2], memo[1][i-1]) + arr[0][i];
+				memo[1][i] = Math.max(memo[0][i-2], memo[0][i-1]) + arr[1][i];
 			}
 			
-			sb.append((Math.max(memo[0][n-1], memo[1][n-1]))).append("\n");
+			sb.append((Math.max(memo[0][n], memo[1][n]))).append("\n");
 		}
 		
 		System.out.println(sb.toString());

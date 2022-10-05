91a047e368868248cb4b4a8d9f4d37f59d116dd4
이현정 <h.jung0409@daum.net>
2022-10-05 22:54:26 +0900

    add BOJ/실버1/포도주 시식

---
+++ 20주차/BOJ_2156.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2156 {
+
+  public static void main(String[] args) throws NumberFormatException, IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+
+  }
+
+}

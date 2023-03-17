95d3fae3791566c325faaa0a984d0f30318e83c5
hyunjung0409 <h.jung0409@daum.net>
2023-03-17 09:41:54 +0900

    add BOJ/실버1/달력/1시간20분

--- 40주차/HJ/BOJ_20207.java
+++ 40주차/HJ/BOJ_20207.java
@@ -8,7 +8,7 @@ public class BOJ_20207 {
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int days[] = new int[366];
+        int days[] = new int[367];
         int N = Integer.parseInt(br.readLine());
 
         StringTokenizer st;
@@ -18,14 +18,32 @@ public class BOJ_20207 {
             start = Integer.parseInt(st.nextToken());
             end = Integer.parseInt(st.nextToken());
 
-            for (int j = start; j < end; j++) {
-                days[j] += 1;
+            for (int j = start; j <= end; j++) {
+                days[j]++;
             }
         }
 
-        int result = 0, length = 0, max = 0;
-        for (int i = 1; i <= days.length; i++) {
-            
+        int result = 0, from = 0, to = 0, max = 0;
+        for (int i = 1; i <= 365; i++) {
+            if(days[i] > 0 && days[i-1] == 0) 
+            {
+                from = i;
+                // System.out.println("from: " + from);
+            }
+            if(days[i] > max) 
+            {
+                max = days[i];
+                // System.out.println("max: " + max);
+            }
+            if(days[i] > 0 && days[i+1] == 0){
+                to = i;
+                // System.out.println("to: " + to);
+                result += (to - from + 1) * max;
+                // System.out.println("result: " + result);
+                max = 0;
+            }
         }
+
+        System.out.println(result);
     }
 }
